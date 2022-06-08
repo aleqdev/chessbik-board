b8a2eb87917069fd8caabc9bdeb9ca3b45bcc6be
necromfox <necromfox@gmail.com>
2022-06-08 10:49:08 +0300

    fix cyclic dep

--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,4 @@
-chessbik_commons::derive_wrapper!(
+chessbik_derive_wrapper::derive_wrapper!(
     #[derive(derive_more::Mul, derive_more::Add, derive_more::Sub, derive_more::Div)]
     #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
     pub struct Eval(pub f32);
--- src/piece_position.rs
+++ src/piece_position.rs
@@ -1,4 +1,4 @@
-chessbik_commons::derive_wrapper!(
+chessbik_derive_wrapper::derive_wrapper!(
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
     pub struct PiecePosition(pub usize);
 );
