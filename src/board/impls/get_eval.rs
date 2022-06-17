a784f9f22c33592d21e5147f3cc96bbaecec59b1
necromfox <necromfox@gmail.com>
2022-06-17 13:32:04 +0300

    board get eval

--- src/board/impls/get_eval.rs
+++ src/board/impls/get_eval.rs
@@ -2,8 +2,16 @@ use crate::{Eval, GetEval};
 
 use super::*;
 
-impl<T: AsRef<Piece> + serde::Serialize> GetEval for Board<T> {
+impl<T> GetEval for Board<T> 
+where T: AsRef<Piece> + serde::Serialize + GetPiece + GetAvailableMoves<T> + Copy + BoardTransform + GetEval
+{
     fn get_eval(&self) -> Eval {
-        todo!()
+        let mut e = Eval(0.);
+
+        for i in 0..54 {
+            e.0 += self.at(i).get_eval().0;
+        }
+
+        e
     }
 }
