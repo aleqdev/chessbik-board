2c439d3ca3bfd9ce5d00d777e0e54f05bd0eacf7
necromfox <necromfox@gmail.com>
2022-06-17 18:39:08 +0300

    fix evals

--- src/piece/ty/impls/get_eval.rs
+++ src/piece/ty/impls/get_eval.rs
@@ -5,12 +5,12 @@ use super::*;
 impl GetEval for PieceTy {
     fn get_eval(&self) -> Eval {
         match self {
-            PieceTy::PAWN => Eval(1.),
+            PieceTy::PAWN => Eval(2.),
             PieceTy::ROOK => Eval(5.),
             PieceTy::KNIGHT => Eval(3.),
             PieceTy::BISHOP => Eval(3.),
             PieceTy::QUEEN => Eval(9.),
-            PieceTy::KING => Eval(0.),
+            PieceTy::KING => Eval(999.),
             PieceTy::MAGE => Eval(3.),
         }
     }
